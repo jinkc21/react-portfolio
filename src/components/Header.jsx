f76b95aa1b933214679f52a79970ba32932c4c47
Jinkc21 <jin.k.choe@gmail.com>
2024-06-18 18:21:05 -0400

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+// components/Header.js
+// import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <h1>Jin Choe</h1>
+      <nav>
+        <NavLink to="/about">About Me</NavLink>
+        <NavLink to="/portfolio">Portfolio</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/resume">Resume</NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
